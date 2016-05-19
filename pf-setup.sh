a88e428ec26c261fbcbab45f2de1409d6933bfed
Andy Tu <andy.tu@originate.com>
2016-05-19 14:11:32 -0700

    Removed temp files

--- pf-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir -p go/src/github.com/Originate/
-cd go/src/github.com/Originate/
-git clone https://github.com/Originate/go_rps.git
-export GOPATH=/workspace/go
-set PATH $PATH $GOPATH
-set PATH $PATH $GOPATH/bin
